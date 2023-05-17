1ee26e92eed740e7d886c19e4b13cd31be3e9306
win0509 <dbtmdfl1109@naver.com>
2023-05-17 12:45:45 +0900

    관리자 페이지 작성중 0517

--- index.php
+++ index.php
@@ -6,20 +6,22 @@
   // print_r($path_name);
   // print_r($path_name[3]);
   $inc_adrs_post = ['signup', 'signin', 'is_signin']; // post로 전달되는 주소
-  $inc_adrs_get = ['signout']; // get으로 전달되는 주소
+  $inc_adrs_get = ['signout', 'check_admin_signin']; // get으로 전달되는 주소
 
   for($i = 0; $i < count($inc_adrs_post); $i++){
     // echo $inc_adrs_post[$i];
-    if($path_name[3] == 'register' && $path_name[4] == $inc_adrs_post[$i] && $_SERVER['REQUEST_METHOD'] == "POST"){
+    if($path_name[2] == 'register' && $path_name[3] == $inc_adrs_post[$i] && $_SERVER['REQUEST_METHOD'] == "POST"){
       include $_SERVER['DOCUMENT_ROOT'].'/baexang_back/register/'.$inc_adrs_post[$i].'.php';
     } 
   }
 
-  if($path_name[3] == 'register' && $path_name[4] == 'signout' && $_SERVER['REQUEST_METHOD'] == "GET"){
-    include $_SERVER['DOCUMENT_ROOT'].'/baexang_back/register/signout.php';
+  for($i = 0; $i < count($inc_adrs_get); $i++){
+  if($path_name[2] == 'register' && $path_name[3] == $inc_adrs_get[$i] && $_SERVER['REQUEST_METHOD'] == "GET"){
+    include $_SERVER['DOCUMENT_ROOT'].'/baexang_back/register/'.$inc_adrs_get[$i].'.php';
   }
+}
 
-  if(!in_array($path_name[4], $inc_adrs_post) && $path_name[4] != 'signout'){ // 배열에 없는 문자열이 나오면 index.php로 전달
+  if(!in_array($path_name[3], $inc_adrs_post) && !in_array($path_name[3], $inc_adrs_get)){ // 배열에 없는 문자열이 나오면 index.php로 전달
     echo 'index.php';
   }
 
---
+++ register/check_admin_signin.php
@@ -0,0 +1,14 @@
+<?php 
+
+    session_start();
+    $msg = [];
+    if(!isset($_SESSION['userid']) && isset($_SESSION['userlvl'] )!= 1){
+        $msg = ['acs_code' => 0];
+    } else{
+        $msg = ['acs_code' => 1];
+    }
+
+    echo json_encode($msg);
+
+
+?>
